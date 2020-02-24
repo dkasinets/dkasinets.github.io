be357688d0dbba1ff55889ecc0555decb3ec4928
Dzmitry Kasinets <dmitryledger@gmail.com>
2020-02-23 21:13:26 -0500

    Fixed bug with loader animation.

--- js/loader.js
+++ js/loader.js
@@ -7,11 +7,14 @@ function setLoader() {
     $( window ).on("load", function() {
         // hide loaders
         $(".loader_first").slideUp();
-        setTimeout(() => {$(".loader_second").slideUp();}, 500);
-
-        // make body scrollable
-        $("body").css("overflow", "visible");
-        // hide loader overlay
-        $(".loader_wrapper").css("display", "none");
+        setTimeout(() => {$(".loader_second").slideUp(onLoaderAnimationFinished);}, 500);
     });
+}
+
+
+function onLoaderAnimationFinished() {
+    // make body scrollable
+    $("body").css("overflow", "visible");
+    // hide loader overlay
+    $(".loader_wrapper").css("display", "none");
 }
