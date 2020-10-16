f0e540ccec2ca9d67d7b36a41c2c2bfacfb72e75
Dzmitry Kasinets <dmitryledger@gmail.com>
2020-10-15 23:52:23 -0400

    Fixing bug with freeze caused by caching.

--- js/loader.js
+++ js/loader.js
@@ -1,21 +1,20 @@
-getLoader();
-setLoader();
+$( ".loader_wrapper" ).ready(function() {
+    getLoader();
+});
+
+
+$( window ).on("load", function() {
+    $(".spinner").fadeOut("fast");
+    $(".loader_wrapper").css("background-color", "transparent");
+    // hide loaders
+    $(".loader_first").slideUp(onFirstAnimationComplete);
+});
 
 
 function getLoader() {
     // TODO: To update Relative File Path later 
     $.get( "/html/loader.html", function( data ) {
         $( ".loader_wrapper" ).html( data );
-    });
-}
-
-
-function setLoader() {
-    $( window ).on("load", function() {
-        $(".spinner").fadeOut("fast");
-        $(".loader_wrapper").css("background-color", "transparent");
-        // hide loaders
-        $(".loader_first").slideUp(onFirstAnimationComplete);
     });
 }
 
