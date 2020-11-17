46317818ddb297f6ecece82951bb0fcb5a8cf7d8
Dzmitry Kasinets <dmitryledger@gmail.com>
2020-11-16 21:40:10 -0500

    Reverted back to 50b1d214b75... stable version to avoid loader bug rabbit hole.

--- js/loader.js
+++ js/loader.js
@@ -1,43 +1,21 @@
-
-$( ".loader_wrapper" ).ready(function() {
-    getLoader();
-    getLoaderOnLoad();
-    getLoaderOnBeforeunload();
-});
-
-
-function getLoaderOnBeforeunload() {
-    $( window ).on('beforeunload', function(){
-        console.log("beforeunload: " + Date.now() + " exists: " + $( ".spinner" ).length)
-        $(".spinner").fadeOut("fast");
-        $(".loader_wrapper").css("background-color", "transparent");
-        // hide loaders
-        $(".loader_first").slideUp(onFirstAnimationComplete);
-    });
-}
-
-
-function getLoaderOnLoad() {
-    $( window ).on("load", function() {
-        console.log("load: " + Date.now() + " exists: " + $( ".spinner" ).length)
-        $(".spinner").fadeOut("fast");
-        $(".loader_wrapper").css("background-color", "transparent");
-        // hide loaders
-        $(".loader_first").slideUp(onFirstAnimationComplete);
-    });
-}
+getLoader();
+setLoader();
 
 
 function getLoader() {
-    console.log("getLoader: " + Date.now() + " Exists: " + $( ".loader_wrapper" ).length);
-
-    $.get( "/html/loader.html")
-    .done(function( data ) {
+    // TODO: To update Relative File Path later 
+    $.get( "/html/loader.html", function( data ) {
         $( ".loader_wrapper" ).html( data );
-        // getLoaderOnLoad();
-    })
-    .fail(function( data ){
-        onLoaderAnimationFinished();
+    });
+}
+
+
+function setLoader() {
+    $( window ).on("load", function() {
+        $(".spinner").fadeOut("fast");
+        $(".loader_wrapper").css("background-color", "transparent");
+        // hide loaders
+        $(".loader_first").slideUp(onFirstAnimationComplete);
     });
 }
 
@@ -48,8 +26,6 @@ function onFirstAnimationComplete() {
 
 
 function onLoaderAnimationFinished() {
-    console.log("onLoaderAnimationFinished: " + Date.now() + " Exists: " + $( ".loader_wrapper" ).length);
-
     // make body scrollable
     $("body").css("overflow", "visible");
     // hide loader overlay
